3ac13fbd24181de56bf9992ee73ec3629ad8dee3
LegionAro <u@example.com>
2024-09-23 23:39:17 +0300

    main page

--- js/main.js
+++ js/main.js
@@ -116,3 +116,96 @@ const swiper4 = new Swiper('.slider', {
     bulletActiveClass: 'active',
   },
 });
+
+const swiper5 = new Swiper('.project-content__gallery', {
+  slidesPerView: 1,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+    speed: 5000,
+  },
+
+  pagination: {
+    el: '.project-content__gallery-pagination-current',
+    type: 'fraction',
+  },
+
+  navigation: {
+    prevEl: '.project-content__gallery-arrow--prev',
+    nextEl: '.project-content__gallery-arrow--next',
+  },
+});
+
+const swiper6 = new Swiper('.acordeon__slider', {
+  slidesPerView: 3,
+
+  spaceBetween: 5,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    600: {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: '.project-content__gallery-pagination-current',
+    type: 'fraction',
+  },
+
+  navigation: {
+    prevEl: '.project-content__gallery-arrow--prev',
+    nextEl: '.project-content__gallery-arrow--next',
+  },
+});
+
+//smooth
+
+const scrollSmoothLinck = document.querySelectorAll('*[data-scroll-smooth]');
+
+for (let elem of scrollSmoothLinck) {
+  elem.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    let blockID = elem.getAttribute('data-scroll-smooth');
+    let top = document.getElementById(blockID).getBoundingClientRect().top;
+
+    document.querySelector('html,body').scrollTo({
+      top: top + window.pageYOffset - 100,
+      behavior: 'smooth',
+    });
+  });
+}
+
+function acordeon(group, all = false, classActive = 'active', itemActive = false) {
+  const acordeons = document.querySelectorAll(`[data-acordeon-group=${group}]`);
+
+  if (acordeons.length > 0) {
+    acordeons.forEach((item) => {
+      const btn = item.querySelector('[data-acordeon-btn]');
+      btn.addEventListener('click', () => handleItemAcordion(item, all));
+    });
+
+    if (itemActive) {
+      acordeons[itemActive - 1].querySelector('[data-acordeon-btn]').click();
+    }
+  }
+
+  function handleItemAcordion(item, all) {
+    if (all === true) {
+      if (item.classList.contains(classActive)) {
+        item.classList.remove(classActive);
+      } else {
+        acordeons.forEach((acordeon) => acordeon.classList.remove(classActive));
+        item.classList.add(classActive);
+      }
+    } else {
+      item.classList.toggle(classActive);
+    }
+  }
+}
+
+acordeon('one', false, 'active');
